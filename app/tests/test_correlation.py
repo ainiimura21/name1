6940b843014569c649da4b57df85bac26cc6edb4
kborowsky25 <kilianborowsky@gmail.com>
2025-01-13 23:06:28 +0000

    added volcano test

--- app/tests/test_correlation.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib.ticker import LogLocator, NullFormatter
-import pytest
-from unittest.mock import patch, mock
-from app.plots.Correlation import plot_correlation
