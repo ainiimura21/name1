02cd891fab507d6dac713b4e851736ecf7e82483
Lina Amraoui <lina.amraoui22@imperial.ac.uk>
2025-01-13 22:34:32 +0000

    style: add explanatory comments for clarity

--- tests/test_getEntrezGeneSymbol.py
+++ tests/test_getEntrezGeneSymbol.py
@@ -1,18 +1,16 @@
 import unittest
 from app.dataloader import getEntrezGeneSymbol  
 
-
-
 class TestgetEntrezGeneSymbol(unittest.TestCase):
 
     def test_getEntrezGeneSymbol_geneid(self):
-        """Test geneID = 1415 correspond to GeneSymbol =CRYBB2 """
+        # Test geneID = 1415 correspond to GeneSymbol =CRYBB2
         self.assertEqual(getEntrezGeneSymbol('EntrezGeneID','1415'), 'CRYBB2')
 
-        """Test that GeneSymbol gives back the same GeneSymbol """
+        # Test that GeneSymbol gives back the same GeneSymbol
         self.assertEqual(getEntrezGeneSymbol('EntrezGeneSymbol','GUCA1A'), 'GUCA1A')
 
-        """Test with targetFullName """
+        # Test with targetFullName
         self.assertEqual(getEntrezGeneSymbol('TargetFullName','Beclin-1'), 'BECN1')
 
         
