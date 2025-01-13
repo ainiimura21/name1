6e2b1ed2fc3c5eb3bafadc61c5ef4ee496899114
ainiimura21 <146862179+ainiimura21@users.noreply.github.com>
2025-01-13 22:29:37 +0000

    tests: add test_correlation

---
+++ tests/test_correlation.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.ticker import LogLocator, NullFormatter
+import pytest
+from unittest.mock import patch, mock
+from app.Correlation import plot_correlation
